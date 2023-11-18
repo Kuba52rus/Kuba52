f1d45969233885c21e35834ab1e39986cb3f7e87
Kuba52rus <Kuba52rus@gmail.com>
2023-11-18 21:05:51 +0300

    It my first commit

--- src/FirstStep.java
+++ src/FirstStep.java
@@ -1,5 +1,11 @@
 public class FirstStep {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a = 5;
+        int b = 6;
+        System.out.println("Сумма = " + getSum(a, b));
     }
-}
+
+        public static int getSum(int num1, int num2){
+            return num1 + num2;
+        }
+    }
