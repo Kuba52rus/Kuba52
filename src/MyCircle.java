a835243a0c36c408206d37005597fcc45f83d1c4
Kuba52rus <Kuba52rus@gmail.com>
2023-11-18 22:20:39 +0300

    It my second commit

---
+++ src/MyCircle.java
@@ -0,0 +1,11 @@
+public class MyCircle {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        if (a>b)
+            System.out.println(a);
+        else
+            System.out.println(b);
+    }
+}
